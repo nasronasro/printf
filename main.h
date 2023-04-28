c2184e683fa0f98f1f488bf24e338130d2347ab2
nasronasro <ahfir49481@gmail.com>
2023-04-28 00:22:32 -0700

    main.h

--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -55,3 +58,4 @@ char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 
+#endif
